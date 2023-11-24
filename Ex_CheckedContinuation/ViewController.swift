68cd44ff77ef15d9fd459b997b236042f8ad5dd8
choiyj <choiyj@vp.co.kr>
2023-11-24 10:25:25 +0900

    init

--- Ex_CheckedContinuation/ViewController.swift
+++ Ex_CheckedContinuation/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let url = "https://random.imagecdn.app/500/150"
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        requestImage { data in
+            print(data)
+        }
+        
+        Task {
+            do {
+                let data = try await requestImageAsycAwait()
+            } catch {
+                print(error)
+            }
+        }
+    
     }
-
+        
+    func requestImage(handler: @escaping ((Data) -> Void)) {
+        let request = URLRequest(url: URL(string: url)!)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            print(error)
+            handler(data!)
+        }.resume()
+    }
+    
+    func requestImageAsycAwait() async throws -> Data {
+        let request = URLRequest(url: URL(string: url)!)
+        
+        return try await withUnsafeThrowingContinuation { continuation in
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let data {
+                    //continuation.resume(returning: data)
+                } else if let error {
+                    //continuation.resume(throwing: error)
+                }
+            }.resume()
+        }
+    }
 
 }
 
